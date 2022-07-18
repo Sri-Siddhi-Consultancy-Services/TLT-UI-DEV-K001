dddf7d06b40f7ea91af984125ffd796d56de2a25
kumarraja <kumar.pedagopu@outlook.com>
2022-07-18 09:54:33 +0530

    more array method examples added

--- js/examples/js-basics/arrays.js
+++ js/examples/js-basics/arrays.js
@@ -21,7 +21,7 @@ const myInts = [1, 2, 3, 4];
 const mySquare = [1, 4, 9, 16];
 
 const myStrArr = "mango,apple,orange".split(",");
-console.log("arrays...", { myNewArr, mysecondArray, myStrArr });
+// console.log("arrays...", { myNewArr, mysecondArray, myStrArr });
 
 /* 
 Create a string from an array
@@ -34,40 +34,42 @@ Create a string from an array
 
 // console.log(str1.concat(' ', str2));
 // expected output: "Hello World"
-const str3 = fruitName[0].concat(",", fruitName[1]);
-const str4 = str3.concat(",", fruitName[2]);
-const str5 = str4.concat(",", fruitName[3]);
+// const str3 = fruitName[0].concat(",", fruitName[1]);
+// const str4 = str3.concat(",", fruitName[2]);
+// const str5 = str4.concat(",", fruitName[3]);
 
 let myStr = "";
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < fruitName.length; i++) {
   myStr = myStr + fruitName[i] + ",";
 }
 // console.log("myStr", myStr);
 
 function makeString(arr, separator) {
-  let sep = separator ? separator : ",";
-  // let list = [];
-  // for(let i=0; i < arr.lenghth; i++){
-  // list += (arr[i])
-  // }
-  // console.log(list.split(" ").toString())
-  // console.log(arr.toString());
+  /* 
+  @arr: array
+  @separator: string
+  */
+  // let sep = separator ? separator : ",";
+  let sep = separator || ",";
+
   let myStr = "";
   for (let i = 0; i < arr.length; i++) {
-    // if (i !== 0) {
-    //   myStr += ",";
-    // }
-    // myStr = myStr + arr[i];
-    // myStr += arr[i];
-    /* 
-    // second method
-      // if (i !== arr.length-1) {
-      //   // myStr = myStr + ",";
-      //   myStr += ",";
-      // }
-     */
-    myStr += i !== 0 ? sep + arr[i] : arr[i]; // one line code
+    //   // if (i !== 0) {
+    //   //   myStr += ",";
+    //   // }
+    //   // myStr = myStr + arr[i];
+    //   // myStr += arr[i];
+    //   /*
+    //   // second method
+    //     // if (i !== arr.length-1) {
+    //     //   // myStr = myStr + ",";
+    //     //   myStr += ",";
+    //     // }
+    //    */
+    // myStr += i !== arr.length - 1 ? arr[i] + separator : arr[i]; // if we add separator at the end
+    myStr += i !== 0 ? sep + arr[i] : arr[i]; // one line code // if we add separator at start
   }
+
   return myStr;
 }
 
@@ -80,25 +82,55 @@ Access an array item by its index
 =============================================================
 */
 
+const fruitName2 = ["mango", "apple", "orange", "grapes", "apple"];
+console.log(fruitName2[1]);
+
 /* 
 Find the index of an item in an array
 =============================================================
 */
+// const appIndex = fruitName2.findIndex((item) => item === "grapes");
+const appIndex = fruitName2.findIndex(function (item) {
+  return item === "apple";
+});
+console.log("appIndex: ", appIndex, fruitName2[appIndex]);
 
 /* 
 Check if an array contains a certain item
 =============================================================
 */
+console.log("is apple available: ", fruitName2.includes("apple"));
 
 /* 
 Append an item to an array
 =============================================================
 */
-
+fruitName2.push("banana");
+console.log("fruites", fruitName2);
+fruitName2.unshift("pineapple");
+console.log("fruites", fruitName2);
 /* 
 Remove the last item from an array
 =============================================================
 */
+
+fruitName2.pop();
+fruitName2.pop();
+console.log("fruites", fruitName2);
+fruitName2.shift();
+fruitName2.shift();
+fruitName2.shift();
+console.log("fruites", fruitName2);
+
+/* 
+Array.prototype.filter()
+
+*/
+
+const mynums = [1, 2, 3, 4, 5, 10, 15, 20, "hi", true, "hello"];
+const newArray = mynums.filter((item) => item < 5);
+
+console.log("newArray", mynums, newArray);
 
 /* Array.prototype.forEach() */
 const myFunc = (n) => console.log(n);
